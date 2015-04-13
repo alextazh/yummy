3486bcdfd16b850344447e7ef7a2dc5ad185c3b3
Alexander Tazh <alexander.tazh@gmail.com>
2015-04-13 17:12:21 +0400

    added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 		gutil = require('gulp-util');
+		coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-	gutil.log('workflows kick ass!');
+gulp.task('coffee', function() {
+	gulp.src('src/coffee/*.coffee')
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('public/javascripts'))
+		)
 });
